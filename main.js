6aa1451c7b23b45b1d75636bd74633c74e5c3e68
BasemAmr <67838239+BasemAmr@users.noreply.github.com>
2020-07-25 19:02:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-var btn = $("button");
+
+// Toggle Celebrate Section On Button Click
+
+var btn = $("#doIt");
 btn.click(()=>{
-	$("section").toggle('slow')
+	$("section").toggle('slow', ()=>{
+		$("section span").toggle('400')
+	})
+});
+
+// Adjust "wrapper" Height
+
+$(".wrapper").height($(window).height());
+
+$(window).resize(function(){
+	$(".wrapper").height($(window).height());
 });
